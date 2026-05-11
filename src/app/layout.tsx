45497fa158cacd0ae4214fc6ad8fb4afebce3df0
Mohan <173985403+mohan-codebase@users.noreply.github.com>
2026-05-11 11:32:13 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,19 +72,11 @@ export default function RootLayout({
           <GlobalModal />
         </ModalProvider>
         <Script id="zsiqchat" strategy="afterInteractive">
-          {`
-            var $zoho=$zoho || {};
-            $zoho.salesiq = $zoho.salesiq || {
-                          widgetcode: "siq8dc889cb6780acd2bfd30fc91d07dcdb44bae525f81bfa66e5d65f1c2414880e016070d80d383b405e205f9433dd516b", 
-
-              values: {},
-              ready: function() {}
-            };
-          `}
+          {`window.$zoho=window.$zoho || {};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}`}
         </Script>
         <Script
           id="zsiqscript"
-          src="https://salesiq.zoho.in/widget"
+          src="https://salesiq.zohopublic.in/widget?wc=siq8dc889cb6780acd2bfd30fc91d07dcdb44bae525f81bfa66e5d65f1c2414880e016070d80d383b405e205f9433dd516b"
           strategy="afterInteractive"
           defer
         />
